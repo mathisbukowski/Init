ee2eb67bcfde9a0266bd34fdda521c13a3c13222
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-08-03 22:35:14 +0200

    feat(makefile_init): adding the makefile init in the bash script

--- init.sh
+++ init.sh
@@ -1,1 +1,37 @@
 #!/bin/bash
+
+PROJECT=$1
+# INIT MAKEFILE
+
+if [ -z "${PROJECT}" ]; then
+    echo "No value for the project name. Please retry."
+    exit 1
+fi
+
+echo "##
+## EPITECH PROJECT, 2024
+## ${PROJECT}
+## File description:
+## Here is a description
+##
+
+SRC = src/main.c
+
+NAME = ${PROJECT}
+
+CFLAGS =  -Wall -Wextra -g
+
+all : \$(NAME)
+
+\$(NAME) :
+\tgcc \$(CFLAGS) \$(SRC) -o \$(NAME) -I./include/
+
+clean:
+\trm -f \$(OBJ)
+
+fclean: clean
+\trm -f \$(NAME)
+
+re: fclean all" > Makefile
+
+echo "Makefile successfully created for ${PROJECT} project.\n"
