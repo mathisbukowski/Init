03530136018284e818cc382341b5ab9fcee0a281
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-08-16 17:44:50 +0200

    feat(templates): adding the c template

---
+++ templates/c/src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** ${PROJECT}
+** File description:
+** Here is a description
+*/
+
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+    printf("Hello World");
+    return 0;
+}
